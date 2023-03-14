d2b159dc7d607ed0ff31f3a0271c64df4f60ef61
Selma <selmabenkaza@gmail.com>
2023-03-14 16:05:41 +1100

    afternoon updates

--- app.js
+++ app.js
@@ -12,7 +12,39 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el));
 
-function arrow(current, next) {
+function arrow(arrow, readme, next) {
     document.getElementById(next).classList.remove('hide');
-    document.getElementById(current).classList.add('hide');
+    document.getElementById(arrow).classList.add('hide');
+    document.getElementById(readme).classList.add('hide');
+}
+
+function nav(item) {
+
+    switch (item) {
+        case 'profile':
+            document.getElementById('profile').classList.remove('hide');
+            document.getElementById('question-one').classList.add('hide');
+            document.getElementById('question-two').classList.add('hide');
+            document.getElementById('supporting-documents').classList.add('hide');
+            break;
+        case 'question-one':
+            document.getElementById('profile').classList.add('hide');
+            document.getElementById('question-one').classList.remove('hide');
+            document.getElementById('question-two').classList.add('hide');
+            document.getElementById('supporting-documents').classList.add('hide');
+            break;
+        case 'question-two':
+            document.getElementById('profile').classList.add('hide');
+            document.getElementById('question-one').classList.add('hide');
+            document.getElementById('question-two').classList.remove('hide');
+            document.getElementById('supporting-documents').classList.add('hide');
+            break;
+        case 'supporting-documents':
+            document.getElementById('profile').classList.add('hide');
+            document.getElementById('question-one').classList.add('hide');
+            document.getElementById('question-two').classList.add('hide');
+            document.getElementById('supporting-documents').classList.remove('hide');
+            break;
+
+    }
 }
